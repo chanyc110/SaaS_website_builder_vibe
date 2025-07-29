8b87aa4c8843f62d6a77654757df815ab5f98caa
chanyc110 <119403040+chanyc110@users.noreply.github.com>
2025-07-29 15:10:10 +0800

    AI jobs

--- vibe/src/inngest/functions.ts
+++ vibe/src/inngest/functions.ts
@@ -1,15 +1,22 @@
+import { openai, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "30s");
+  async ({ event}) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets.",
+      model: openai({ model: "gpt-4o"}),
+    });
 
-    await step.sleep("wait-a-moment", "10s");
+    const {output} = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`,
+    );
+    console.log(output)
 
-    await step.sleep("wait-a-moment", "5s");
-
-    return { message: `Hello ${event.data.name}!` };
+    return { output };
   },
 );
--- vibe/src/trpc/routers/_app.ts
+++ vibe/src/trpc/routers/_app.ts
@@ -6,14 +6,14 @@ export const appRouter = createTRPCRouter({
     invoke: baseProcedure  // invoke background job(inngest) from trpc
         .input(
             z.object({
-                text: z.string(),
+                value: z.string(),
             })
         )
         .mutation(async({input}) => {
             await inngest.send({
                 name: "test/hello.world",
                 data: {
-                    name: input.text
+                    value: input.value
                 }
             })
             return {ok: "success"};
